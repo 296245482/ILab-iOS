053d11b685b49db982d2e74e74635f37ec38b539
Cheng Long <296245482@qq.com>
2016-12-16 12:19:32 +0800

    update with xcode8

---
+++ Pods/Target Support Files/Pods-whisky/Pods-whisky-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_whisky : NSObject
+@end
+@implementation PodsDummy_Pods_whisky
+@end
---
+++ Pods/Target Support Files/UIActivityIndicator-for-SDWebImage+UIButton/UIActivityIndicator-for-SDWebImage+UIButton-umbrella.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+#import "UIButton+UIActivityIndicatorForSDWebImage.h"
+#import "UIImageView+UIActivityIndicatorForSDWebImage.h"
+#import "UIView+UIActivityIndicatorForSDWebImage.h"
+
+FOUNDATION_EXPORT double UIActivityIndicator_for_SDWebImage_UIButtonVersionNumber;
+FOUNDATION_EXPORT const unsigned char UIActivityIndicator_for_SDWebImage_UIButtonVersionString[];
+
